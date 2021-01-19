2e0de0117042a07a186ef73644998fa7ca398a37
ishii287 <62539453+ishii287@users.noreply.github.com>
2021-01-19 13:17:16 +0530

    frontend added

---
+++ frontend/src/core/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../styles.css"
+import {API} from "../backend"
+import Base from './Base';
+
+
+export default function Home() {
+    console.log("API is ", API);
+    return (
+        <Base title="Home Page">
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+
+            </div>
+        </Base>
+    )
+}
